366b42a08abdb2867480c4fa20eee1e31c5c1dc2
Varonex <development.varonex@gmail.com>
2025-11-22 03:53:18 +0100

    Rewrote everything in C#

--- StateMachine/StateMachine/Player/player_state.gd
+++
@@ -1,8 +0,0 @@
-@abstract class_name PlayerState extends State
-
-@export var player: Player
-
-func get_context() -> Dictionary[String, Variant]:
-	return {
-		"speed": player.speed
-	}
--- StateMachine/StateMachine/state_transition.gd
+++
@@ -1,5 +0,0 @@
-@abstract class_name StateTransition extends Resource
-
-@export var target_state: NodePath
-
-@abstract func can_transition(context: Dictionary[String, Variant]) -> bool
